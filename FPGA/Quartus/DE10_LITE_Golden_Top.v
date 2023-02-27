4ac53d0f861f60e04d8c5be60d7bfee6cc74ffb6
splogdes <95136830+splogdes@users.noreply.github.com>
2023-02-27 23:07:14 +0000

    buttons, hex and switches

--- FPGA/Quartus/DE10_LITE_Golden_Top.v
+++ FPGA/Quartus/DE10_LITE_Golden_Top.v
@@ -76,13 +76,21 @@ module DE10_LITE_Golden_Top(
 //=======================================================
 
 nios_accelerometer u0 (
-	.clk_clk                                            (MAX10_CLK1_50),  // clk.clk
-	.reset_reset_n                                      (1'b1),           // reset.reset_n
-	.led_external_connection_export                     (LEDR[9:0]),      // led_external_connection.export
 	.accelerometer_spi_external_interface_I2C_SDAT      (GSENSOR_SDI),    // accelerometer_spi_external_interface.I2C_SDAT
 	.accelerometer_spi_external_interface_I2C_SCLK      (GSENSOR_SCLK),   // .I2C_SCLK
 	.accelerometer_spi_external_interface_G_SENSOR_CS_N (GSENSOR_CS_N),   // .G_SENSOR_CS_N
-	.accelerometer_spi_external_interface_G_SENSOR_INT  (GSENSOR_INT [1]) // .G_SENSOR_INT
+	.accelerometer_spi_external_interface_G_SENSOR_INT  (GSENSOR_INT [1]), // .G_SENSOR_INT
+	.buttons_external_connection_export                 (KEY),        //          buttons_external_connection.export
+	.clk_clk                                            (MAX10_CLK1_50),  // clk.clk
+	.led_external_connection_export                     (LEDR[9:0]),      // led_external_connection.export
+	.reset_reset_n                                      (1'b1),           // reset.reset_n
+	.switches_external_connection_export                (SW),                //         switches_external_connection.export
+	.hex_5_external_connection_export                   (HEX5),                   //            hex_5_external_connection.export
+	.hex_0_external_connection_export                   (HEX0),                   //            hex_0_external_connection.export
+	.hex_1_external_connection_export                   (HEX1),                   //            hex_1_external_connection.export
+	.hex_2_external_connection_export                   (HEX2),                   //            hex_2_external_connection.export
+	.hex_3_external_connection_export                   (HEX3),                   //            hex_3_external_connection.export
+	.hex_4_external_connection_export                   (HEX4)                    //            hex_4_external_connection.export	
 );
 
 endmodule
